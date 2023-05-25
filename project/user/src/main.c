3cabdb99c8972788b2b7ffe737fcd3482a5c78c5
huzife <634763349@qq.com>
2023-05-26 00:42:43 +0800

    mm32f5277 freeRTOS project template

---
+++ project/user/src/main.c
@@ -0,0 +1,26 @@
+// includes
+#include "zf_common_headfile.h"
+
+// defines
+
+// variables
+
+// functions
+
+// code
+int main (void)
+{
+    clock_init(SYSTEM_CLOCK_120M);                                              // 初始化芯片时钟 工作频率为 120MHz
+    debug_init();                                                               // 初始化默认 Debug UART
+
+    // 此处编写用户代码 例如外设初始化代码等
+    
+    // 此处编写用户代码 例如外设初始化代码等
+
+    while(1)
+    {
+        // 此处编写需要循环执行的代码
+        
+        // 此处编写需要循环执行的代码
+    }
+}
